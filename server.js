6468d6fc4eb45fe67e7519d35d4b405837012c4c
prashant <pksingh706586@gmail.com>
2021-04-29 18:21:42 +0530

    initial

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+
+const app = express();
+
+//port 
+const port= process.env.PORT || 5000;
+
+//provide static files
+app.use(express.static('public'));
+
+//Templating Engine 
+app.engine('hbs',exphbs({ extname: '.hbs'}));
+app.set('view engine','hbs');
+
+
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+//listening to port
+app.listen(port,()=>{
+    console.log(`server up and running on the port ${port}`);
+})
